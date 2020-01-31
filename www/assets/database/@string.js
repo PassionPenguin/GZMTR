9d2a909e59283a37cff9b0981af6058c533c54e7
PassionPenguin <penguin.zhang@qq.com>
2020-01-31 15:12:50 +0800

    add localize support

---
+++ www/assets/database/@string.js
@@ -0,0 +1,19 @@
+/* Made by Penguin */
+let f_name = [
+    ["廣州地鐵車站信息", "广州地铁车站信息", "", "GZMTR STATIONS' INFO"],
+    ["您想要前往", "您想要前往", "", "You'd like to go to...'"],
+    ["主頁", "主页", "", "HOME"],
+    ["車站信息", "车站信息", "", "INFO"],
+    ["運營線路圖", "运营线路图", "", "ROUTEMAP"],
+    ["個人", "个人", "", "ACCOUNT"],
+    ["請選擇你嘅乘車方向", "请选择您的乘车方向", "", "Selcet Your Direction"],
+    ["設施與出入口", "设备与出入口", "", "Facilities and Exits"],
+    ["車站地圖", "车站地图", "", "MAP"],
+    ["維基百科", "维基百科", "", "WIKI"],
+    ["簡體中文", "简体中文", "", "CHINESE SIMPLIFIED"],
+    ["繁體中文", "繁体中文", "", "CHINESE TRADITIONAL"],
+    ["英文", "英语", "", "ENGLISH"],
+    ["警告，所有改動將會立即影響整個應用", "警告，所有改动将会立即影响整个应用", "", "WARNING, ALL CHANGES WILL EFFECT THE WHOLE APPLICATION"],
+    ["用戶界面語言", "用户界面语言", "", "UI Languages"],
+    ["警告，所有改動將會立即影響整個應用", "警告，所有改动将会立即影响整个应用", "", "WARNING, ALL CHANGES WILL EFFECT THE WHOLE APPLICATION"]
+];
