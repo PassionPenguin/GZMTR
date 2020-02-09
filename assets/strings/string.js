95726c4a7a3b7be1cf125412c326b460f45dd28b
PassionPenguin <penguin.zhang@qq.com>
2020-02-09 14:06:33 +0800

    init index.html finished

---
+++ assets/strings/string.js
@@ -0,0 +1,5 @@
+/* Made by Penguin */
+
+window.sentences = [
+    "就算你failed佐，我都唔會唔鍾意你[icon]fa fa-heart beating[/icon]","就算你失败了，我也会爱你呀[icon]fa fa-heart beating[/icon]","[icon]fa fa-heart beating[/icon]","[icon]fa fa-heart beating[/icon]","No matter you successed or not,\n I will love you all the time[icon]fa fa-heart beating[/icon]"
+];
