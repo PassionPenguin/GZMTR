c0dd443a2982b17b3c7ccfb8c7eb10e6f060fb9c
PassionPenguin <penguin.zhang@qq.com>
2020-02-19 17:55:42 +0800

    upd stringlist

--- assets/strings/string.js
+++ assets/strings/string.js
@@ -4,3 +4,8 @@ window.sentences = [
     ["就算你failed佐，我都唔會唔鍾意你[icon]fa fa-heart beating[/icon]", "就算你失败了，我也会爱你呀[icon]fa fa-heart beating[/icon]", "[icon]fa fa-heart beating[/icon]", "[icon]fa fa-heart beating[/icon]", "No matter you successed or not,\n I will love you all the time[icon]fa fa-heart beating[/icon]"],
     ["今日系[icon]fa fa-lemon' style='font-weight:400[/icon] 仲系 [icon]fa fa-apple-alt[/icon] 嘞", "今天是[icon]fa fa-lemon' style='font-weight:400[/icon] 还是 [icon]fa fa-apple-alt[/icon] 呢", "[icon]fa fa-lemon[/icon]", "[icon]fa fa-lemon[/icon]", "What'll be today? [icon]fa fa-lemon' style='font-weight:400[/icon] or [icon]fa fa-apple-alt[/icon] ?"]
 ];
+window.string = {
+    locationDesc: ["地理座標", "地理坐标", "", "", "Map Position"],
+    lineDesc: ["經過線路", "经过线路", "", "", "Line Passed"],
+    codeDesc: ["站點代碼", "站点代码", "", "", "Station Code"],
+};
