91d851a033bce56f20f65f6f84208c2b7d999aa6
HumanBeingXenon <903120109@qq.com>
2020-02-22 12:38:04 +0800

    Update string.js

--- assets/strings/string.js
+++ assets/strings/string.js
@@ -4,7 +4,7 @@ window.sentences = [
     ["就算你failed佐，我都唔會唔鍾意你[icon]fa fa-heart beating[/icon]", "就算你失败了，我也会爱你呀[icon]fa fa-heart beating[/icon]", "[icon]fa fa-heart beating[/icon]", "[icon]fa fa-heart beating[/icon]", "No matter you succeeded or not,\n I will love you all the time[icon]fa fa-heart beating[/icon]"],
     ["今日系[icon]fa fa-lemon' style='font-weight:400[/icon] 仲系 [icon]fa fa-apple-alt[/icon] 嘞", "今天是[icon]fa fa-lemon' style='font-weight:400[/icon] 还是 [icon]fa fa-apple-alt[/icon] 呢", "[icon]fa fa-lemon[/icon]", "[icon]fa fa-lemon[/icon]", "What will be today? [icon]fa fa-lemon' style='font-weight:400[/icon] or [icon]fa fa-apple-alt[/icon] ?"]
 ];
-window.feature = [["主页", "主页", "", "", "Home"], ["信息", "信息", "", "", "Info"], ["地图", "地图", "", "", "Routemap"], ["设置", "设置", "設定", "", "Settings"]];
+window.feature = [["主頁", "主页", "ホーム", "홈페이지", "Home"], ["車站資訊", "车站信息", "駅の情報", "역 정보", "Info"], ["線路圖", "线路图", "路線図", "운영 로드맵", "Routemap"], ["設置", "设置", "設定", "설치", "Settings"]];
 window.string = {
     infoDesc: ["简介信息", "简介信息", "案内", "", "Info"],
     facilityDesc: ["车站设施", "车站设施", "ファシリティーズ", "", "Facilities"],
