e5694d6954afb7a54a5f4d491f33d3da9d9fa2d0
PassionPenguin <penguin.zhang@qq.com>
2020-02-27 15:03:31 +0800

    support thread icon support login mod

--- assets/scripts/blog.required.js
+++ assets/scripts/blog.required.js
@@ -55,21 +55,152 @@ window.loadURL = (url) => {
 };
 
 window.forumDisplay = () => {
+    window.blog = [{name: "北京区", fid: 7, iconid: "BJ", enName: "Beijing"}, {
+        name: "天津区",
+        fid: 6,
+        iconid: "TJ",
+        enName: "Tianjin"
+    }, {name: "上海区", fid: 8, iconid: "SH", enName: "Shanghai"}, {
+        name: "广州区",
+        fid: 23,
+        iconid: "GZ",
+        enName: "Guangzhou"
+    }, {name: "长春区", fid: 40, iconid: "CCH", enName: "Changchun"}, {
+        name: "大连区",
+        fid: 41,
+        iconid: "DL",
+        enName: "Dalian"
+    }, {name: "武汉区", fid: 39, iconid: "WH", enName: "Wuhan"}, {
+        name: "重庆区",
+        fid: 38,
+        iconid: "CQ",
+        enName: "Chongqing"
+    }, {name: "深圳区", fid: 24, iconid: "SZ", enName: "Shenzhen"}, {
+        name: "南京区",
+        fid: 22,
+        iconid: "NJ",
+        enName: "Nanjing"
+    }, {name: "成都区", fid: 53, iconid: "CD", enName: "Chengdu"}, {
+        name: "沈阳区",
+        fid: 50,
+        iconid: "SY",
+        enName: "Shenyang"
+    }, {name: "佛山区", fid: 56, iconid: "FS", enName: "Foshan"}, {
+        name: "西安区",
+        fid: 54,
+        iconid: "XA",
+        enName: "Xi'an"
+    }, {name: "苏州区", fid: 51, iconid: "SUZ", enName: "Suzhou"}, {
+        name: "昆明区",
+        fid: 70,
+        iconid: "KM",
+        enName: "Kunming"
+    }, {name: "杭州区", fid: 52, iconid: "HZ", enName: "Hangzhou"}, {
+        name: "哈尔滨区",
+        fid: 55,
+        iconid: "HAB",
+        enName: "Harbin"
+    }, {name: "郑州区", fid: 64, iconid: "ZZ", enName: "Zhengzhou"}, {
+        name: "长沙区",
+        fid: 67,
+        iconid: "CS",
+        enName: "Changsha"
+    }, {name: "宁波区", fid: 65, iconid: "NB", enName: "Ningbo"}, {
+        name: "无锡区",
+        fid: 68,
+        iconid: "WX",
+        enName: "Wuxi"
+    }, {name: "青岛区", fid: 66, iconid: "QD", enName: "Qingdao"}, {
+        name: "南昌区",
+        fid: 71,
+        iconid: "NC",
+        enName: "Nanchang"
+    }, {name: "福州区", fid: 72, iconid: "FZ", enName: "Fuzhou"}, {
+        name: "东莞区",
+        fid: 75,
+        iconid: "DG",
+        enName: "Dongguan"
+    }, {name: "南宁区", fid: 73, iconid: "NN", enName: "Nanning"}, {
+        name: "合肥区",
+        fid: 74,
+        iconid: "HF",
+        enName: "Hefei"
+    }, {name: "石家庄区", fid: 140, iconid: "SJZ", enName: "Shijiazhuang"}, {
+        name: "贵阳区",
+        fid: 76,
+        iconid: "GY",
+        enName: "Guiyang"
+    }, {name: "厦门区", fid: 77, iconid: "XM", enName: "Xiamen"}, {
+        name: "乌鲁木齐区",
+        fid: 143,
+        iconid: "UM",
+        enName: "Urumqi"
+    }, {name: "温州区", fid: 142, iconid: "WZ", enName: "Wenzhou"}, {
+        name: "济南区",
+        fid: 148,
+        iconid: "JN",
+        enName: "Jinan"
+    }, {name: "兰州区", fid: 78, iconid: "LZ", enName: "Lanzhou"}, {
+        name: "常州区",
+        fid: 48,
+        iconid: "CZ",
+        enName: "Changzhou"
+    }, {name: "徐州区", fid: 144, iconid: "XZ", enName: "Xuzhou"}, {
+        name: "呼和浩特区",
+        fid: 151,
+        iconid: "HH",
+        enName: "Huhhot"
+    }, {name: "香港区", fid: 28, iconid: "HK", enName: "Hongkong"}, {
+        name: "澳门区",
+        fid: 79,
+        iconid: "MO",
+        enName: "Macau"
+    }, {name: "台湾区", fid: 36, iconid: "TW", enName: "Taiwan"}, {
+        name: "海外区",
+        fid: 47,
+        iconid: "HW",
+        enName: "Oversea"
+    }, {name: "综合区", fid: 37, iconid: "ZOH", enName: "Comprehensive"}, {
+        name: "轨道收藏",
+        fid: 33,
+        iconid: "COL",
+        enName: "Collections"
+    }, {name: "都市风情", fid: 16, iconid: "SCN", enName: "City Style"}, {
+        name: "地铁美食",
+        fid: 15,
+        iconid: "FOOD",
+        enName: "Food"
+    }, {name: "轨交游戏", fid: 145, iconid: "GAME", enName: "Games"}, {
+        name: "站前广场",
+        fid: 21,
+        iconid: "CHAT",
+        enName: "H2O Plaza"
+    }, {name: "站务公告", fid: 17, iconid: "MA", enName: "Announcement"}];
     let app = cE({type: "div", attr: [["id", "pg-app"]]});
     app.append(cE({type: "div", attr: [["class", "pg-backgroundImage"]]}));
     app.append(cE({type: "div", attr: [["class", "pg-backgroundFilter"]]}));
+    let fid = Int(getPara("fid", pg.$("link[rel='alternate']")[0].href));
     {
+        let curFid = blog.map(i => i.fid);
+        let curIndex = curFid.indexOf(fid);
         let appbar = cE({type: "div", attr: [["id", "pg-app-titleBar"]]});
         let appName = cE({type: "div", attr: [["class", "pg-titleName"]]});
-        appName.append(cE({type: "div", attr: [["class", "city-icon-GZ"]]}));
+        appName.append(cE({
+            type: "img",
+            attr: [["class", "city-icon-GZ"], ["src", "http://127.0.0.1/assets/media/images/city-icon/compressed/" + blog[curIndex].iconid + ".svg"]]
+        }));
         appName.append(cE({
             type: "div",
-            innerHTML: "<span class='cn'>广州区</span><span class='en'>Guangzhou</span>",
+            innerHTML: "<span class='cn'>" + blog[curIndex].name + "</span><span class='en'>" + blog[curIndex].enName + "</span>",
             attr: [["class", "fidName"]]
         }));
         appbar.append(appName);
         app.append(appbar);
-        let topName = cE({type: "div", innerText: "广州区", attr: [["id", "topName"]]});
+        let topName = cE({
+            type: "div",
+            innerHTML: "<span onclick='loadURL(\"http://www.ditiezu.com\")' class='mi theme-color ic-back'>chevron_left</span><span>" + blog[curIndex].name + "</span>",
+            attr: [["id", "topName"]]
+        });
         app.append(topName);
         app.onscroll = () => {
             if (app.scrollTop > 100)
@@ -167,7 +298,11 @@ window.threadDisplay = () => {
     lastPage = Int(lastPage.includes("...") ? lastPage.substr(4) : lastPage);
     let app = cE({type: "div", attr: [["id", "pg-app"]]});
     let threadSubject = pg.$("#thread_subject")[0].innerText;
-    let topName = cE({type: "div", innerText: threadSubject, attr: [["id", "topName"], ["class", "scrolled"]]});
+    let topName = cE({
+        type: "div",
+        innerHTML: "<span onclick='loadURL(\"http://www.ditiezu.com\")' class='mi theme-color ic-back'>chevron_left</span><span>" + threadSubject + "</span>",
+        attr: [["id", "topName"], ["class", "scrolled"]]
+    });
     app.append(topName);
     let threadPost = [];
     threadPost = [...pg.$("div[id^='post_']>table[id^='pid']")].map((i, index) => [i.children[0].children, index]);
@@ -286,6 +421,18 @@ window.threadDisplay = () => {
         }
         app.append(pgsBox)
     }
+    document.body.append(app);
+};
+
+window.loginDisplay = () => {
+    pg.$("#main_hnav a.xi2")[0].click();
+    let topName = cE({
+        type: "div",
+        innerHTML: "<span onclick='loadURL(\"http://www.ditiezu.com\")' class='mi theme-color ic-back'>chevron_left</span><span>登录地铁族</span>",
+        attr: [["id", "topName"], ["class", "scrolled"]]
+    });
+    let app = cE({type: "div", attr: [["id", "pg-app"]]});
+    app.append(topName);
     document.body.append(app);
 };
 
