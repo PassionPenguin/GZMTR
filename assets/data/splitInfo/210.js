4b3cc83878b4a793e5dd646e8698047842b2e514
HumanBeingXenon <903120109@qq.com>
2020-03-01 01:14:00 +0800

    Update 210.js

--- assets/data/splitInfo/210.js
+++ assets/data/splitInfo/210.js
@@ -1,1 +1,1 @@
-let s_inf_210={"name":["東平","东平","東平-とうへい","둥핑","Dongping"],"inf":[{"platform":1,"facilities":[[9,[1,2,3],"A"],[17,[1],"B"]]},{"platform":2,"facilities":[[1,[3],"A"],[2,[1,2],"B"]]}],"location":[113.317841,23.25117],"via":["GF"],"platformBelong":[["1","GF"],["2","GF"]]};
+let s_inf_210={"name":["東平","东平","東平-とうへい","둥핑","Dongping"],"inf":[{"platform":1,"facilities":[[9,[1,2,3],"A"],[17,[1],"B"]]},{"platform":2,"facilities":[[1,[3],"A"],[2,[1,2],"B"]]}],"location":[113.12917,22.9652520],"via":["GF"],"platformBelong":[["1","GF"],["2","GF"]]};
