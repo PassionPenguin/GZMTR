6014322b60d6ff0d78ab7911feca8318a622c21e
HumanBeingXenon <903120109@qq.com>
2020-03-01 01:12:04 +0800

    splitInfo added
    Added 237~245.

---
+++ assets/data/splitInfo/239.js
@@ -0,0 +1,13 @@
+let s_inf_239 = { 
+    "name": ["琶醍", "琶醍", "琶醍-はだい", "", "Party Pier"], 
+    "inf": [{ 
+        "platform": 1, 
+        "facilities": [] 
+    }, { 
+        "platform": 2, 
+        "facilities": [] 
+    }], 
+    "location": [113.340009,23.107736], 
+    "via": ["THZ1"], 
+    "platformBelong": [["2", "THZ1"], ["1", "THZ1"]] 
+};
---
+++ assets/data/splitInfo/240.js
@@ -0,0 +1,13 @@
+let s_inf_241 = { 
+    "name": ["會展西", "会展西", "會展西-かいてんにし", "", "Canton Fair Complex West"], 
+    "inf": [{ 
+        "platform": 1, 
+        "facilities": [] 
+    }, { 
+        "platform": 2, 
+        "facilities": [] 
+    }], 
+    "location": [113.348647,23.105679], 
+    "via": ["THZ1"], 
+    "platformBelong": [["2", "THZ1"], ["1", "THZ1"]] 
+};
